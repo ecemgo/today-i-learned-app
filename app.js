be45c3a50d055793a2752ad68e923fd729e9328a
Ecem Gokdogan <ecemgo@hotmail.com>
2023-08-15 16:13:00 +0300

    updated form elements

---
+++ app.js
@@ -0,0 +1,6 @@
+// const shareFact = document.getElementById("share-fact");
+
+// //! get the cursor position in the input
+// shareFact.addEventListener("keyup", (e) => {
+//   console.log("Caret at: ", e.target.selectionStart);
+// });
