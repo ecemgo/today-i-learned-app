fd91aeb96b6f414ddb22cfa1264be152f30682d6
Ecem Gokdogan <ecemgo@hotmail.com>
2023-08-16 22:17:23 +0300

    displaying and hiding the form

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-// const shareFact = document.getElementById("share-fact");
+const shareButton = document.querySelector(".btn-share");
+const factForm = document.querySelector(".fact-form");
 
-// //! get the cursor position in the input
-// shareFact.addEventListener("keyup", (e) => {
-//   console.log("Caret at: ", e.target.selectionStart);
-// });
+shareButton.addEventListener("click", function () {
+  if (factForm.classList.contains("hidden")) {
+    factForm.classList.remove("hidden");
+    shareButton.textContent = "Close";
+  } else {
+    factForm.classList.add("hidden");
+    shareButton.textContent = "Share a fact";
+  }
+});
